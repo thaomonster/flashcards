dcd98fceab110893b22200c307c37107047af27e
Thao Ma <MaThao@gmail.com>
2020-11-30 16:06:00 -0700

    Add properties to class and passed card-test

--- src/Card.js
+++ src/Card.js
@@ -1,6 +1,9 @@
 class Card {
-  constructor() {
-
+  constructor(id, question, possibleAnswer, correctAnswer) {
+    this.id = id;
+    this.question =  question;
+    this.answers = possibleAnswer;
+    this.correctAnswer = correctAnswer;
   };
 };
 
