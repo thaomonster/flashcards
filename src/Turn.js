e90f68e8f61a9db09fb40d5d8a5ddce9ec3e21d5
Thao Ma <MaThao@gmail.com>
2020-11-30 19:40:32 -0700

    Add test and methods for returnGuess, returnCard, and evaluateGuess

--- src/Turn.js
+++ src/Turn.js
@@ -1,8 +1,23 @@
+const Card = require("./Card");
+
 class Turn {
-  constructor() {
-    
-  }
-}
+  constructor(userGuess, card) {
+    this.userGuess = userGuess;
+    this.card = card;
+  };
+
+  returnGuess() {
+    return this.userGuess;
+  };
+
+  returnCard() {
+    return this.card;
+  };
+
+  evaluateGuess() {
+    return this.userGuess === this.card.correctAnswer ? true : false
+  };
+};
 
 
 
--- test/Turn-test.js
+++ test/Turn-test.js
@@ -14,7 +14,30 @@ describe('Turn', () => {
   });
 
   it('should be an instance of Turn', () => {
-    const card = new Turn();
+    const turn = new Turn('boolean', card);
     expect(turn).to.be.an.instanceof(Turn);
-  }); 
+  });
+
+  it('should return userGuess', () => {
+    const turn = new Turn('boolean', card);
+    expect(turn.returnGuess()).to.equal('boolean')
+  });
+
+  it('should return current card', () => {
+    const card = new Card(9, 'What does the callback function for find() return?', ['boolean', 'array', 'object'], 'boolean');
+    const turn = new Turn('boolean', card);
+    expect(turn.returnCard()).to.equal(card);
+  });
+
+  it('should compare user guess to correctAnswer and return a boolean value', () => {
+    const card = new Card(9, 'What does the callback function for find() return?', ['boolean', 'array', 'object'], 'boolean');
+    const turn = new Turn('boolean', card);
+    expect(turn.evaluateGuess()).to.equal(true)
+  });
+
+  it('should return a string to tell the user their result', () => {
+    const card = new Card(9, 'What does the callback function for find() return?', ['boolean', 'array', 'object'], 'boolean');
+    const turn = new Turn('boolean', card);
+    expect(turn.giveFeedback()).to.equal('correct!')
+  });
 });
