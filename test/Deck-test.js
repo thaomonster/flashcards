89947b1b9b303ebbb078e122871aa27001746392
Thao Ma <MaThao@gmail.com>
2020-12-01 19:22:19 -0700

    Add chai expect and require and add requires for round, deck, card, turn

--- test/Deck-test.js
+++ test/Deck-test.js
@@ -1,5 +1,6 @@
 const chai = require('chai');
 const expect = chai.expect;
+
 const Deck = require('../src/Deck');
 const Card = require('../src/Card');
 
@@ -24,7 +25,7 @@ describe('Deck', () => {
     const card3 = new Card(3, 'What type of prototype method directly modifies the existing array?', ['mutator method', 'accessor method', 'iteration method'], 'mutator method');
 
     let deck = new Deck([card1, card2, card3]);
-    
+
     expect(deck.countCards()).to.equal(3);
   });
 });
