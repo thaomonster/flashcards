9b6cd40646f9012a4a98636580ce3d961bcc02a6
Thao Ma <MaThao@gmail.com>
2020-12-02 13:45:05 -0700

    create beforeEach for game test

--- test/Game-test.js
+++ test/Game-test.js
@@ -6,3 +6,18 @@ const Deck = require('../src/Deck');
 const Card = require('../src/Card');
 const Game = require('../src/Game');
 
+describe('Game', () => {
+
+  let deck, card1, card2, card3, round, game;
+
+  beforeEach(() => {
+    card1 = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+    card2 = new Card(2, 'What is a comma-separated list of related values?', ['array', 'object', 'function'], 'array');
+    card3 = new Card(3, 'What type of prototype method directly modifies the existing array?', ['mutator method', 'accessor method', 'iteration method'], 'mutator method');
+    deck = new Deck([card1, card2, card3]);
+    round = new Round(deck);
+    game = new Game();
+  });
+
+  
+})
