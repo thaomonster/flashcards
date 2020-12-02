909fe4f76b69cc070a4266779b06e6e9118fc491
Thao Ma <MaThao@gmail.com>
2020-12-01 18:34:03 -0700

    Add and pass function test block

--- test/Deck-test.js
+++ test/Deck-test.js
@@ -3,3 +3,9 @@ const expect = chai.expect;
 const Deck = require('../src/Deck');
 const Card = require('../src/Card');
 
+describe('Deck', () => {
+
+  it('should be a function', () => {
+    expect(Deck).to.be.a('function')
+  });
+});
