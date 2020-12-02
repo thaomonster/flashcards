087452836c8c0248a58c06cc13a94f42ca0c8d50
Thao Ma <MaThao@gmail.com>
2020-12-02 14:36:22 -0700

    Refactor card class to add beforeEach

--- test/Card-test.js
+++ test/Card-test.js
@@ -5,6 +5,12 @@ const Card = require('../src/Card');
 
 describe('Card', function() {
 
+  let card;
+
+  beforeEach(() => {
+     card = new Card(1, 'What do we call when we assign variable declarations a default value of undefined while placing any functions directly in memory?', ['creation', 'execution', 'hoisting'], 'hoisting');
+  });
+
   it('should be a function', function() {
     const card = new Card();
     expect(Card).to.be.a('function');
@@ -16,17 +22,14 @@ describe('Card', function() {
   }); 
 
   it('should store a question', function() {
-    const card = new Card(1, 'What do we call when we assign variable declarations a default value of undefined while placing any functions directly in memory?', ['creation', 'execution', 'hoisting'], 'hoisting');
     expect(card.question).to.equal('What do we call when we assign variable declarations a default value of undefined while placing any functions directly in memory?');
   });  
 
   it('should store a list of possible answers', function() {
-    const card = new Card(1, 'What do we call when we assign variable declarations a default value of undefined while placing any functions directly in memory?', ['creation', 'execution', 'hoisting'], 'hoisting');
     expect(card.answers).to.deep.equal(['creation', 'execution', 'hoisting']);
   });  
 
   it('should store the correct answer', function() {
-    const card = new Card(1, 'What do we call when we assign variable declarations a default value of undefined while placing any functions directly in memory?', ['creation', 'execution', 'hoisting'], 'hoisting');
     expect(card.correctAnswer).to.equal('hoisting');
   });
 });
