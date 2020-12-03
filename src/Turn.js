21f883b6c3e95999607abe98026549e606fb1074
Thao Ma <MaThao@gmail.com>
2020-12-02 21:02:04 -0700

    clean up with eslint Turn.js

--- src/Turn.js
+++ src/Turn.js
@@ -1,27 +1,25 @@
-const Card = require("./Card");
-
 class Turn {
   constructor(userGuess, card) {
     this.userGuess = userGuess;
     this.card = card;
-  };
+  }
 
   returnGuess() {
     return this.userGuess;
-  };
+  }
 
   returnCard() {
     return this.card;
-  };
+  }
 
   evaluateGuess() {
     return this.userGuess === this.card.correctAnswer ? true : false
-  };
+  }
 
   giveFeedback() {
     return this.evaluateGuess() ? 'correct!' : 'incorrect!';
-  };
-};
+  }
+}
 
 
 
