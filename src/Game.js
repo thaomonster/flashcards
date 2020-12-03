5e0237f266e8848f4ad7d45ca69f927a84fe7b39
Thao Ma <MaThao@gmail.com>
2020-12-02 17:31:09 -0700

    Refactor game-test to tak out unwanted items and add startGame() in game.js

--- src/Game.js
+++ src/Game.js
@@ -1,3 +1,7 @@
+const Card = require('./Card');
+const Round = require('../src/Round');
+const Deck = require('../src/Deck');
+
 const data = require('./data');
 const prototypeQuestions = data.prototypeData;
 const util = require('./util');
@@ -8,8 +12,15 @@ class Game {
   };
 
   startGame() {
-    
-  }
+    this.currentRound++
+
+    const cards = prototypeQuestions.map((question) => {
+      return new Card(question.id, question.question, question.possibleAnswer, question.correctAnswer)
+    });
+
+    let deck = new Deck(cards);
+    let round = new Round(deck);
+  };
 
   printMessage(deck, round) {
       console.log(`Welcome to FlashCards! You are playing with ${deck.countCards()} cards.
--- test/Game-test.js
+++ test/Game-test.js
@@ -2,20 +2,14 @@ const chai = require('chai');
 const expect = chai.expect;
 
 const Round = require('../src/Round');
-const Deck = require('../src/Deck');
 const Card = require('../src/Card');
 const Game = require('../src/Game');
 
 describe('Game', () => {
 
-  let deck, card1, card2, card3, round, game;
+  let game;
 
   beforeEach(() => {
-    card1 = new Card(1, 'What do we call when we assign variable declarations a default value of undefined while placing any functions directly in memory?', ['creation', 'execution', 'hoisting'], 'hoisting');
-    card2 = new Card(2, 'What is the phase where the interpreter sets aside some space in memory to store any variables and functions we might need to access?', ['creation phase', 'execution phase', 'hoisting'], 'creation phase');
-    card3 = new Card(3, 'What do we call when JavaScript executes one line at a time?', ['single-threaded', 'accessor method', 'top-bottom'], 'single-threaded');
-    deck = new Deck([card1, card2, card3]);
-    round = new Round(deck);
     game = new Game();
   });
 
@@ -27,9 +21,10 @@ describe('Game', () => {
     expect(game).to.be.an.instanceOf(Game);
   });
 
-  it('should have currentRound default to be 0', () => {
-    expect(game.currentRound).to.equal(0);
-  });
+  it('shoulkd keep track of current round after game starts', () => {
 
-  // it('should ')
+    game.startGame();
+
+    expect(game.currentRound).to.equal(1);
+  });
 });
