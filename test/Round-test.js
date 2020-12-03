b557e0efa8c39c251bf23649ae9a2440f25013b0
Thao Ma <MaThao@gmail.com>
2020-12-02 21:07:24 -0700

    clean up with eslint Round-test.js

--- test/Round-test.js
+++ test/Round-test.js
@@ -4,17 +4,15 @@ const expect = chai.expect;
 const Round = require('../src/Round');
 const Deck = require('../src/Deck');
 const Card = require('../src/Card');
-const Turn = require('../src/Turn');
 
 describe('Round', () => {
-  let card1, card2, card3, deck, turn, round;
+  let card1, card2, card3, deck, round;
   
   beforeEach(() => { 
     card1 = new Card(1, 'What do we call when we assign variable declarations a default value of undefined while placing any functions directly in memory?', ['creation', 'execution', 'hoisting'], 'hoisting');
     card2 = new Card(2, 'What is the phase where the interpreter sets aside some space in memory to store any variables and functions we might need to access?', ['creation phase', 'execution phase', 'hoisting'], 'creation phase');
     card3 = new Card(3, 'What do we call when JavaScript executes one line at a time?', ['single-threaded', 'accessor method', 'top-bottom'], 'single-threaded');
     deck = new Deck([card1, card2, card3]);
-    turn = new Turn();
     round = new Round(deck);
   });
 
